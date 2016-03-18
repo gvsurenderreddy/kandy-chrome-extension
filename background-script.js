f2d2f3310248f6d879f3a8940c8cab753f27c060
MichelSimonot <Mitch.Simonot@kandy.io>
2016-03-18 10:09:29 -0400

    Changes to allow screen sharing in an iframe.

--- background-script.js
+++ background-script.js
@@ -2,6 +2,11 @@
 function onMessageExternal(request, sender, sendResponse) {
     if (request && request.message) {
         if (request.message === 'chooseDesktopMedia') {
+
+            // Make sure that the tab url matches the url of the frame requesting the media. This
+            // is necessary to run screensharing inside of an iframe, such as on Codepen.
+            sender.tab.url = sender.url;
+
             chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], sender.tab, function (mediaSourceId) {
                 if (!mediaSourceId || !mediaSourceId.length) {
                     sendResponse({
